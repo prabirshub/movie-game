24772f182045ecca3cf8cdcec9a742ae5468d4d9
Prabir Singh <prabirsinghdigital@gmail.com>
2023-07-01 01:14:04 +0530

    security update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: '5234a13c',
         s: searchTerm,
@@ -44,7 +44,7 @@ createAutoComplete({
 let leftMovie
 let rightMovie
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: '5234a13c',
       i: movie.imdbID,
